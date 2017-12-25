eff943e71d31204aed937e8c6d76e992b780dd8a
David Szotten <davidszotten@gmail.com>
2017-12-25 12:55:49 +0000

    refactor date/time handling

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, time
+from datetime import datetime, timedelta, time, date
 import csv
 import json
 
@@ -24,12 +24,14 @@ def parse():
     data = list(reader)
     entries = []
     for row in data:
-        date = datetime.strptime(row['Date'], '%m-%d-%Y')
-        weekday = date.weekday()
-        start = datetime.strptime(row['Start'].replace('00:', '12:'), '%I:%M %p')
+        datetime_str = '{Date} {Start}'.format(**row)
+        # export contains malformed am/pm times (e.g. "00:01 am")
+        datetime_str = datetime_str.replace('00:', '12:')
+        start = datetime_str
+        start = datetime.strptime(datetime_str, '%m-%d-%Y %I:%M %p')
+        weekday = start.weekday()
         duration = int(row['Duration [min]'])
         entries.append({
-            'date': date,
             'duration': duration,
             'start': start,
         })
@@ -39,6 +41,10 @@ def parse():
 def write_json(entries):
     with open('data.json', 'w') as handle:
         json.dump(entries, handle, indent=4)
+
+
+def time_diff(start, end):
+    return datetime.combine(date.min, end) - datetime.combine(date.min, start)
 
 
 def write_svg(entries):
@@ -64,17 +70,21 @@ def write_svg(entries):
         first = entries[0]
         last = entries[-1]
 
-        first_start = datetime.strptime('0', '%H')
-        first_date = first['date']
-        last_date = last['date']
+        first_start = first['start']
+        base = datetime.combine(first_start.date(), time())
+        last_start = last['start']
 
         seconds_per_day = timedelta(days=1).total_seconds()
 
-        def date_y(date):
-            return (date - first_date).total_seconds() / seconds_per_day * 25
+        def date_y(dt):
+            return (
+                dt.date() - base.date()
+            ).total_seconds() / seconds_per_day * 25
 
-        def time_x(time):
-            return (time - first_start).total_seconds() /seconds_per_day * svg_width + 100
+        def time_x(dt):
+            return time_diff(
+                base.time(), dt.time()
+            ).total_seconds() /seconds_per_day * svg_width + 100
 
         def minute_width(minute):
             return svg_width * minute / (24*60)
@@ -82,21 +92,21 @@ def write_svg(entries):
         for offset in [0, svg_width / 2]:
             handle.write(
                 '<rect class="day-part" x="{x}" y="{y}" width="{width}" height="{height}"/>\n'.format(
-                    x=time_x(first_start) + offset,
-                    y=date_y(first_date),
+                    x=time_x(base) + offset,
+                    y=date_y(base),
                     width=minute_width(60*6),
-                    height=date_y(last_date),
+                    height=date_y(last_start),
                 )
             )
 
         for entry in entries:
-            y = date_y(entry['date'])
+            y = date_y(entry['start'])
             #  import pdb; pdb.set_trace()
             x = time_x(entry['start'])
             width = minute_width(entry['duration'])
             handle.write(
                 ' <text x="10" y="{y}" font-size="15" font-family="Georgia">{date}</text>\n'.format(
-                    date=entry['date'].date(),
+                    date=entry['start'].date(),
                     y=y + 20,
                 )
             )
